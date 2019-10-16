025e7769b9cedd6422cc3981befabbec4616bfac
tintheanh <tintheanh@gmail.com>
2019-10-15 17:23:34 -0700

    revert changes

--- src/components/Activity/Team/Team.js
+++ src/components/Activity/Team/Team.js
@@ -14,28 +14,29 @@ export default class Team extends Component {
 	renderTeamMems() {
 		const { showmore } = this.state;
 		const { teammems } = this.props;
-		if (teammems.length > 3) {
-			return (
-				<div className="row inline-columns">
-					{teammems.map((mem, i) => {
-						if (!showmore) {
-							if (i < 3) {
-								return <TeamMem key={mem.id} mem={mem} />;
-							}
-							return null;
-						}
-						return <TeamMem key={mem.id} mem={mem} />;
-					})}
-					<div className="col-lg-12" style={{ marginTop: 35 }}>
-						<div style={{ margin: 'auto', textAlign: 'center' }}>
-							<button className="showmore" onClick={this.toggleShow}>
-								{!showmore ? 'Show more' : 'Show less'}
-							</button>
-						</div>
-					</div>
-				</div>
-			);
-		}
+		// if (teammems.length > 3) {
+		// 	return (
+		// 		<div className="row inline-columns">
+		// 			{teammems.map((mem, i) => {
+		// 				// if (!showmore) {
+		// 				// 	if (i < 3) {
+		// 				// 		return <TeamMem key={mem.id} mem={mem} />;
+		// 				// 	}
+		// 				// 	return null;
+		// 				// }
+		// 				// return <TeamMem key={mem.id} mem={mem} />;
+		// 				return <TeamMem key={mem.id} mem={mem} />;
+		// 			})}
+		// 			{/* <div className="col-lg-12" style={{ marginTop: 35 }}>
+		// 				<div style={{ margin: 'auto', textAlign: 'center' }}>
+		// 					<button className="showmore" onClick={this.toggleShow}>
+		// 						{!showmore ? 'Show more' : 'Show less'}
+		// 					</button>
+		// 				</div>
+		// 			</div> */}
+		// 		</div>
+		// 	);
+		// }
 		return (
 			<div className="row inline-columns">
 				{this.props.teammems.map((mem) => <TeamMem key={mem.id} mem={mem} />)}
@@ -49,7 +50,7 @@ export default class Team extends Component {
 			<section
 				id="team"
 				className="front-page-section"
-				style={{ paddingBottom: 0, paddingTop: 65, background: '#fff' }}
+				style={{ paddingBottom: 40, paddingTop: 65, background: '#fff' }}
 			>
 				<div className="section-header" style={{ marginBottom: 55 }}>
 					<div className="container">
