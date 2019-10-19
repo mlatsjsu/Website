390ce37f372f984dae74256e6cbd9dcc9ec867eb
Andrew Jong <andrewjong87@gmail.com>
2019-10-19 16:35:35 -0700

    Add more to get involved
    Benefits of official membership.
    Say join our slack

--- src/components/GetInvolved/GetInvolved.js
+++ src/components/GetInvolved/GetInvolved.js
@@ -63,7 +63,15 @@ export default class GetInvolved extends Component {
 								<div className="col-sm-12">
 									<h3 className="title">Get Involved</h3>
 									<h5 style={{ fontSize: 16, color: 'rgb(119, 119, 119)', marginTop: 22 }}>
-										Complete one from below per semester
+										First, make sure to join our 
+										<a href="sjsumlclub.slack.com/signup">Slack group </a> 
+										for notifications about upcoming events!
+										<br/>
+										Our meetings will always be open to all SJSU students. 
+										However, as an official member, you get extra benefits 
+										such as getting featured on our website, priority for 
+										RSVP events, and club funding for projects. 
+										Complete at least one item below per semester for official membership:
 									</h5>
 								</div>
 							</div>
