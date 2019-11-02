2995bcbb445913323ac57044dee560673200575d
tintheanh <tintheanh@gmail.com>
2019-11-02 14:58:19 -0700

    update get involved section & fix some bugs

--- src/components/GetInvolved/GetInvolved.js
+++ src/components/GetInvolved/GetInvolved.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { SLACKAPIKEY } from '../../config/apiKey';
 
 export default class GetInvolved extends Component {
 	state = { rules: [], numberOfUsers: 0, slack: '' };
 	async componentDidMount() {
 		const [ ruleRes, userRes, slackRes ] = await Promise.all([
 			fetch('https://sjsuml-cms.herokuapp.com/getinvolveds'),
-			fetch(`https://slack.com/api/users.list?token=${SLACKAPIKEY}&include_locale=true&pretty=1`),
+			fetch(
+				`https://slack.com/api/users.list?token=${process.env.REACT_APP_API_KEY}&include_locale=true&pretty=1`
+			),
 			fetch('https://sjsuml-cms.herokuapp.com/contacts')
 		]);
 
